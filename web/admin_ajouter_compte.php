ebc7991949326c25281191ccb2f1d483a1565173
Antoine PAJDAK <antoine.pajdak@isen-ouest.yncrea.fr>
2023-05-02 15:02:56 +0200

    02-05 Pajdak

--- web/admin_ajouter_compte.php
+++ web/admin_ajouter_compte.php
@@ -25,10 +25,10 @@
     </header>
     
     <?php
-        require('../lib/fonction.php');  
-        ini_set('display_errors', 1);
-        error_reporting(E_ALL);
-        $db = dbConnect();
+        //require('../lib/fonction.php');  
+        //ini_set('display_errors', 1);
+        //error_reporting(E_ALL);
+        //$db = dbConnect();
     ?>
 
 
